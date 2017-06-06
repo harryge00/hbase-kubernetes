76bb741dd59723c63cc2c6b5d648abbedac34084
haoyuan <haoyuan.ge@caicloud.io>
2017-06-06 22:15:45 +0800

    update yamls

--- README.md
+++ README.md
@@ -10,9 +10,11 @@ Distributed hbase based on hdfs above kubernetes with 2 masters and 2 regionserv
 
 Download hbase from `https://archive.apache.org/dist/hbase/1.2.3/hbase-1.2.3-bin.tar.gz` and copy it into hbase-image
 ```
-# docker build -t index.caicloud.io/caicloud/hbase:sohu hbase-image
-# kubectl create -f hbase.yaml
+# cd hbase-image
+# IMAGE_BASE_URL=cargo.caicloudprivatetest.com/caicloud IMAGE_TAG=lastest make
+# cd ..
+# kubectl create -f hmaster.yaml
+# kubectl create -f region.yaml
 ```
-
 
 Feel free to file a issue
--- hbase-image/start-k8s-hbase.sh
+++ hbase-image/start-k8s-hbase.sh
@@ -1,34 +1,27 @@
 #!/bin/bash
 
 export HBASE_CONF_FILE=/opt/hbase/conf/hbase-site.xml
-export HADOOP_USER_NAME=hdfs
+export HADOOP_USER_NAME=root
 export HBASE_MANAGES_ZK=false
 
-sed -i "s/@HBASE_MASTER_PORT@/$HBASE_MASTER_PORT/g" $HBASE_CONF_FILE
-sed -i "s/@HBASE_MASTER_INFO_PORT@/$HBASE_MASTER_INFO_PORT/g" $HBASE_CONF_FILE
-sed -i "s/@HBASE_REGION_PORT@/$HBASE_REGION_PORT/g" $HBASE_CONF_FILE
-sed -i "s/@HBASE_REGION_INFO_PORT@/$HBASE_REGION_INFO_PORT/g" $HBASE_CONF_FILE
-sed -i "s/@HDFS_PATH@/$HDFS_SERVICE:$HDFS_PORT\/$ZNODE_PARENT/g" $HBASE_CONF_FILE
+sed -i "s/@HDFS_PATH@/$HDFS_PATH/g" $HBASE_CONF_FILE
 sed -i "s/@ZOOKEEPER_IP_LIST@/$ZOOKEEPER_SERVICE_LIST/g" $HBASE_CONF_FILE
 sed -i "s/@ZOOKEEPER_PORT@/$ZOOKEEPER_PORT/g" $HBASE_CONF_FILE
 sed -i "s/@ZNODE_PARENT@/$ZNODE_PARENT/g" $HBASE_CONF_FILE
-cat /dev/null > /opt/hbase/conf/regionservers
-for i in ${HBASE_REGION_LIST[@]}
-do
-   arr=(${i//:/ })
-   echo "${arr[1]}" >> /opt/hbase/conf/regionservers
-done
 
-for i in ${HBASE_MASTER_LIST[@]}
+# set fqdn
+for i in $(seq 1 10)
 do
-   arr=(${i//:/ })
-   echo "${arr[0]} ${arr[1]}" >> /etc/hosts
-done
-
-for i in ${HBASE_REGION_LIST[@]}
-do
-   arr=(${i//:/ })
-   echo "${arr[0]} ${arr[1]}" >> /etc/hosts
+    if grep --quiet $CLUSTER_DOMAIN /etc/hosts; then
+        break
+    elif grep --quiet $POD_NAME /etc/hosts; then
+        cat /etc/hosts | sed "s/$POD_NAME/${POD_NAME}.${POD_NAMESPACE}.svc.${CLUSTER_DOMAIN} $POD_NAME/g" > /etc/hosts.bak
+        cat /etc/hosts.bak > /etc/hosts
+        break
+    else
+        echo "waiting for /etc/hosts ready"
+        sleep 1
+    fi
 done
 
 if [ "$HBASE_SERVER_TYPE" = "master" ]; then
